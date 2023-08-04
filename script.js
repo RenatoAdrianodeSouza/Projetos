42ab7fced51a1f89fcf832ad9b9c887582b61c84
Renato <renatounico@hotmail.com>
2023-08-04 20:37:00 -0300

    Correção de erros

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleMode() {
     const html = document.documentElement
-    html.classList.toggle('light')
+    html.classList.toggle("light")
 
     const img = document.querySelector("#profile img")
 
-    if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/renato_light.png')
+    if (html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/renato_light.png")
     } else {
-        img.setAttribute('scr', './assets/renato.png')
+        img.setAttribute("src", "./assets/renato.png")
     }
-}
+} 
